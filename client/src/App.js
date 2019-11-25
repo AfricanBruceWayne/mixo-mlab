528894c05f165a2024f0810f7156bf8c4b905222
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-11-25 20:24:55 +0200

    Updated project

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,12 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
 
-import Home from './components/Home';
-
-import SignUp from './components/auth/Signup';
-import Login from './components/auth/Login';
-
+import AppNavbar from './components/AppNavbar';
 
 function App() {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">Sign-Up</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/register">
-                        <SignUp />
-                    </Route>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-
-            </div>
-        </Router>
+        <div>
+            <AppNavbar />
+        </div>
     );
 }
 
