f1bac52316a29996af011c088ccd9d06bac1d005
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-11-28 12:16:19 +0200

    Updated server

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const chalk = require('chalk');
+const session = require('express-session');
 const logger = require('morgan');
+const cors  =   require('cors');
 const mongoose = require('mongoose');
 
+var isProduction = process.env.NODE_ENV === 'production';
+
 /*
-*   Load environment variables
+*  Load environment variables
 */
 
 dotenv.config({ path: '.env' });
@@ -15,7 +19,7 @@ dotenv.config({ path: '.env' });
 * Connect to Mongo
 */
 
-//Set up default mongoose connection
+// Set up default mongoose connection
 const uri = process.env.MLAB_URI;
 mongoose.connect(uri, { 
     useUnifiedTopology: true,
@@ -35,14 +39,24 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, '%s MongoDB connection error: ', chalk.red('✗')));
 
-// Create Express server
+// Create global app object
 const app = express();
 
+// Normal express config defaults
+app.use(cors());
+app.use(logger('dev'));
 // Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1209600000 }
+}));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production')
+if (isProduction)
 {
     // Set static folder
     app.use(express.static('client/build'));
@@ -52,22 +66,38 @@ if (process.env.NODE_ENV === 'production')
     });
 }
 
-/**
+/*
  * Error Handler.
- */
-if (process.env.NODE_ENV === 'development') {
+*/
+
+if (!isProduction) 
+{
     // only use in development
     app.use(errorHandler());
-  } else {
+} else 
+{
     app.use((err, req, res, next) => {
-      console.error('%s ' + err, chalk.red('✗'));
-      res.status(500).send('Server Error');
+        // catch 404 and forward to error handler
+        if (res.status(404)) { 
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        }
+        if (res.status(500)) {
+            console.error('%s ' + err, chalk.red('✗'));
+            res.status(500).send('Server Error');
+            res.json({'errors': {
+                message: err.message,
+                error: {}
+            }});
+        }
     });
-  }
+}
 
 /*
 *   Start Express server
 */
+
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
     console.log('%s App is succesfully running', chalk.green('✓'));
