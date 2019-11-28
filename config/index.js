bc7514ebb2da4ad8b2b3a183563be7a0f7c2c359
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-11-28 12:16:37 +0200

    Added config

---
+++ config/index.js
@@ -0,0 +1,28 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/User');
+
+passport.use(new LocalStrategy({
+    usernameField: 'user[email]',
+    passwordField: 'user[password]'
+}, (email, passport, done) => {
+    User.findOne({ email: email }).then((user) => {
+        if (!user || !user.validPassword(passport)) {
+            return done(null, false, {
+                errors: {'email or password': 'is invalid'}
+            });
+        }
+
+        return done(null, user);
+    }).catch(done);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+  
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
