dce0ff8bdefffe538600ecf0103380255887484a
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-12-05 12:11:39 +0200

    Updated config

--- config/index.js
+++ config/index.js
@@ -1,28 +1,5 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/User');
-
-passport.use(new LocalStrategy({
-    usernameField: 'user[email]',
-    passwordField: 'user[password]'
-}, (email, passport, done) => {
-    User.findOne({ email: email }).then((user) => {
-        if (!user || !user.validPassword(passport)) {
-            return done(null, false, {
-                errors: {'email or password': 'is invalid'}
-            });
-        }
-
-        return done(null, user);
-    }).catch(done);
-}));
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
+module.exports = {
+    secret: process.env.NODE_ENV === 'production' ? process.env.SESSION_SECRET : 'secret'
+};
   
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
-});
+
