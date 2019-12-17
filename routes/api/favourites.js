c21cab1f026f7ac3fca3ef6639f627400bbee516
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-12-17 12:31:42 +0200

    favourite routes

---
+++ routes/api/favourites.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var User = require('../../models/User');
+var auth = require('../auth');
+  
+// Favorite an cocktail
+router.post('/', auth.required, (req, res, next) => {
+    var cocktailId = req.cocktail._id;
+
+    User.findById(req.payload.id).then((user) => {
+        if (!user) { return res.sendStatus(401); }
+
+        return user.favorite(cocktailId).then(() => {
+        return req.cocktail.updateFavoriteCount().then((cocktail) => {
+            return res.json({cocktail: cocktail.toJSONFor(user)});
+        });
+        });
+    }).catch(next);
+});
+  
+// Unfavorite an cocktail
+router.delete('/:favourite', auth.required, (req, res, next) => {
+    var cocktailId = req.cocktail._id;
+
+    User.findById(req.payload.id).then((user) => {
+        if (!user) { return res.sendStatus(401); }
+
+        return user.unfavorite(cocktailId).then(() => {
+        return req.cocktail.updateFavoriteCount().then((cocktail) => {
+            return res.json({cocktail: cocktail.toJSONFor(user)});
+        });
+        });
+    }).catch(next);
+});
+
+module.exports = router;
+
