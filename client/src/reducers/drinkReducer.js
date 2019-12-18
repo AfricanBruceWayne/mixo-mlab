ddb11723fbb5acd5e4024a5a7e033054db54d365
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-12-18 22:32:48 +0200

    Updated reducers

--- client/src/reducers/drinkReducer.js
+++ client/src/reducers/drinkReducer.js
@@ -1,24 +1,39 @@
 import {
     GET_DRINKS,
+    GET_DRINK,
     ADD_DRINK,
     UPDATE_DRINK,
     DELETE_DRINK,
-    DRINKS_LOADING
+    DRINKS_LOADING,
+    LIKE_DRINK,
+    UNLIKE_DRINK,
+    SUBMIT_COMMENT
 } from '../actions/types';
 
 const initialState = {
     drinks: [],
+    drink: {},
     loading: false
 };
 
 export default function(state = initialState, action) {
     switch (action.type) {
+      case DRINKS_LOADING:
+        return {
+          ...state,
+          loading: true
+        };
       case GET_DRINKS:
         return {
           ...state,
           drinks: action.payload,
           loading: false
         };
+      case GET_DRINK:
+        return {
+          ...state,
+          drink: action.payload
+        }
       case DELETE_DRINK:
         return {
           ...state,
@@ -34,10 +49,26 @@ export default function(state = initialState, action) {
           ...state,
           drinks: state.drinks.filter(drink => drink._id !== action.payload)
         };
-      case DRINKS_LOADING:
+     
+      case LIKE_DRINK:
+      case UNLIKE_DRINK:
+        let index = state.drinks.findIndex(
+          (drink) => drink.drinkId === action.payload.drinkId
+        );
+        state.drinks[index] = action.payload;
+        if (state.drink.drinkId === action.payload.drinkId) {
+          state.drink = action.payload;
+        } 
+        return {
+          ...state
+        };
+      case SUBMIT_COMMENT: 
         return {
           ...state,
-          loading: true
+          drink: {
+            ...state.drink,
+            comments: [action.payload, ...state.drink.comments]
+          }
         };
       default:
         return state;
