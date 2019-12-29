64ff9694ba02e6b6ced9d127a0326ca7cd8de76b
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-12-29 18:54:46 +0200

    Updated client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,56 @@
-import React from 'react'
-import store from './store';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+// Redux
+import store from './redux/store';
 import { Provider } from 'react-redux';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUser } from './redux/actions/userActions';
 
-import AppNavbar from './components/AppNavbar';
+// Components
+import AppNavbar from './components/layout/AppNavbar';
+import themeObject from './util/theme';
+import AuthRoute from './util/AuthRoute';
 
-function App() {
-    return (
-        <Provider store={store}>
-            <div>
-                <AppNavbar />
-            </div>
-        </Provider>
-    );
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import User from './pages/User';
+
+import axios from 'axios';
+
+const theme = createMuiTheme(themeObject);
+
+class App extends Component {
+    
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <Provider store={store}>
+                    <Router>
+                        <AppNavbar />
+                        <div className="container">
+                            <Switch>
+                            <Route exact path="/" component={Home} />
+                                <AuthRoute exact path="/login" component={Login} />
+                                <AuthRoute exact path="/signup" component={Signup} />
+                                <Route exact path="/users/:handle" component={User} />
+                                <Route
+                                exact
+                                path="/users/:handle/drink/:drinkId"
+                                component={User}
+                                />
+                            </Switch>
+                        </div>
+                    </Router>
+                </Provider>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 
